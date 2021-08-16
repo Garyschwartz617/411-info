73b9ddd7e96f6cd15b43910bf11e2cf2653e0210
Garyschwartz617 <gary.s.schwartz617@gmail.com>
2021-08-16 21:53:32 +0300

    week8 day2

--- main/views.py
+++ main/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from .models import Person
+from django.http import Http404
 # Create your views here.
 def homepage(request):
     return render(request,'index.html')
 
 def phone(request,wrd):
-    prs =Person.objects.get(phonenumber = wrd)
-    index = {'person': prs}
-    return render(request,'phone.html', index)
+    try:
+        prs =Person.objects.get(phonenumber = wrd)
+        index = {'person': prs}
+        return render(request,'phone.html', index)
+    except Person.DoesNotExist:
+        raise Http404
+
 
 def name(request,wrd):
-    prs =Person.objects.get(name = wrd)
-    index = {'person': prs}
-    return render(request,'phone.html', index)    
+    try:
+        prs =Person.objects.get(name = wrd)
+        index = {'person': prs}
+        return render(request,'phone.html', index)
+    except Person.DoesNotExist:
+        raise Http404
+            
